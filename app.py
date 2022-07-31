743558c09ee9402b952cba3797bd45036424d4c9
pmayaduque <pmayaduque@gmail.com>
2022-07-31 16:01:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,61 @@ book_lists_id = '17DTbRs54Y-7nntaSqaIFEHAcMNa4ven6OxDqWswT3rE'
 
 url_teachers = f"https://docs.google.com/spreadsheets/d/{book_teachers_id}/gviz/tq?tqx=out:csv"
 url_lists= f"https://docs.google.com/spreadsheets/d/{book_lists_id}/gviz/tq?tqx=out:csv"
-#url_acopios = f"https://docs.google.com/spreadsheets/d/{book_id}/gviz/tq?tqx=out:csv&sheet={sheet_acopios}"
+
 df_teachers= pd.read_csv(url_teachers)
+teachers_list = df_teachers["nombre"].values.tolist()
+teachers_dict = {i:teachers_list[i] for i in  range(len(teachers_list))}
 df_lists= pd.read_csv(url_lists)
-#df_acopios = pd.read_csv(url_acopios)
+
+
+# Define stylesheets
+external_stylesheets = [dbc.themes.BOOTSTRAP,
+    #'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap'
+]
+
+# Create the app
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
+                title="IE Barrio salvador")
+# needed to run in heroku
+server = app.server
+
+app.layout = dbc.Container([
+        #navbar,
+        dbc.Row("Hello"),
+        dbc.Container(
+            dbc.Card([
+                    dbc.CardImg(src="/static/images/placeholder286x180.png", top=True),
+                    dbc.CardBody(
+                        [
+                            html.H4("Card title", className="card-title"),
+                            html.P(
+                                "Some quick example text to build on the card title and "
+                                "make up the bulk of the card's content.",
+                                className="card-text",
+                            ),
+                            dcc.Dropdown(
+                               options=['New York City', 'Montreal', 'San Francisco'],
+                               value='Montreal'
+                            ),
+                            dbc.Button("Go somewhere", color="primary"),
+                        ]
+                    ),
+            ]),
+                      
+                      
+            fluid=True
+        ),
+
+
+        dbc.Container(id="tab-content", className="p-4", fluid=True),
+        #dbc.Row(html.Img(src='assets/images/footnote_federica.png', style={'width':'100%'})),
+    ],
+    fluid=True,
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+    
+
 
