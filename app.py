857bba74e4e70334ec7feae13a46b253d94c54e2
pmayaduque <pmayaduque@gmail.com>
2022-08-03 21:40:03 -0500

    mvp v1.1
    The mvp with all components without comments

--- app.py
+++ app.py
@@ -10,28 +10,19 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_table
 from dash import no_update
 from dash.dependencies import Input, Output, State
 import plotly.express as px
-from plotly import graph_objs as go
-import pygsheets
 import gspread
-import pandas as pd
 from datetime import date
-from plotly.offline import plot
-
-
-
-import gspread
-from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 scopes = ['https://www.googleapis.com/auth/spreadsheets',
           'https://www.googleapis.com/auth/drive']
 
-credentials = Credentials.from_service_account_file('access_key.json', scopes=scopes)
+credentials = Credentials.from_service_account_file('access_key.json', 
+                                                    scopes=scopes)
 
 gc = gspread.authorize(credentials)
 
@@ -43,33 +34,32 @@ book_teachers_id = '1bqZ8OnYfpM0A6EoVoM_YRurfNmn2UCTAzPYdkZk2vaM'
 book_classes_id = '17DTbRs54Y-7nntaSqaIFEHAcMNa4ven6OxDqWswT3rE'
 book_consolidate_id = '1w8h3dzb4dqSGDrj_QThuES7J_aqrAx_nwXgY7qUDSA0'
 
-book_teachers = gc.open_by_key(book_teachers_id)
-worksheet = book_teachers.worksheet('Docentes')
-#url_teachers = f"https://docs.google.com/spreadsheets/d/{book_teachers_id}/gviz/tq?tqx=out:csv"
-#df_teachers= pd.read_csv(url_teachers)
-df_teachers= pd.DataFrame(worksheet.get_all_records())
+# Read data about teachers
+#book_teachers = gc.open_by_key(book_teachers_id)
+#worksheet = book_teachers.worksheet('Docentes')
+url_teachers = f"https://docs.google.com/spreadsheets/d/{book_teachers_id}/gviz/tq?tqx=out:csv"
+df_teachers= pd.read_csv(url_teachers)
+#df_teachers= pd.DataFrame(worksheet.get_all_records())
+df_teachers = df_teachers.sort_values(by=['nombre'])
 teachers_list = df_teachers["nombre"].values.tolist()
 teachers_dict = [{"label": k, "value": k} for k in teachers_list]
 
-
-book_classes = gc.open_by_key(book_classes_id)
-worksheet = book_classes.worksheet('Cursos')
-#url_classes= f"https://docs.google.com/spreadsheets/d/{book_classes}/gviz/tq?tqx=out:csv"
-#df_classes= pd.read_csv(url_classes)
-df_classes= pd.DataFrame(worksheet.get_all_records())
+# Read data about classes
+#book_classes = gc.open_by_key(book_classes_id)
+#worksheet = book_classes.worksheet('Cursos')
+url_classes = f"https://docs.google.com/spreadsheets/d/{book_classes_id}/gviz/tq?tqx=out:csv"
+df_classes= pd.read_csv(url_classes)
+#df_classes= pd.DataFrame(worksheet.get_all_records())
 classes_id = df_classes["curso"].values.tolist()
 classes_list = dict(zip(df_classes.curso, df_classes.google_id))
 classes_dict = [{"label": k, "value": k} for k in classes_list.keys()]
 
-
-# book_class1A = '1TtuUshmaQPrJa1C33v1pec4Cninh98xXqGIebbG9APs'
-# #book_class1A = classes_dict[selec_class]
-# sheet_estudiantes = 'Estudiantes'
-# url_class1A = f"https://docs.google.com/spreadsheets/d/{book_class1A}/gviz/tq?tqx=out:csv&sheet={'Estudiantes'}"
-# df_class1A= pd.read_csv(url_class1A)
-# class1A_list = list(zip(df_class1A.codigo, df_class1A.nombre))
-# class1A_dict = [{"label": k[1], "value": k[0]} for k in class1A_list]
-    
+# Read consolidate data
+url_asistencia = f"https://docs.google.com/spreadsheets/d/{book_consolidate_id}/gviz/tq?tqx=out:csv"
+df_asistencia= pd.read_csv(url_asistencia)
+#book_consolidate = gc.open_by_key(book_consolidate_id)
+#worksheet = book_consolidate.worksheet('Consolidado')
+#df_asistencia = pd.DataFrame(worksheet.get_all_records())
 
 
 # Define stylesheets
@@ -79,27 +69,10 @@ external_stylesheets = [dbc.themes.BOOTSTRAP,
 ]
 
 
-# #authorization
-# gc = pygsheets.authorize(service_file='access_key.json')
-
-# # Create empty dataframe
-# df = pd.DataFrame()
-
-# # Create a column
-# df['name'] = ['John', 'Steve', 'Sarah']
-
-# #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-# sh = gc.open('Asistencia')
-
-# #select the first sheet 
-# wks = sh[0]
-
-# #update the first sheet with df, starting at cell B2. 
-# wks.set_dataframe(df,(3,2))
-
 # Create the app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 title="IE Barrio salvador")
+
 # needed to run in heroku
 server = app.server
 
@@ -112,6 +85,7 @@ app.layout = dbc.Container([
                 html.P(
                     "Complete la información del curso al cual registrará la asistencia.",
                     className="card-text",
+                    id='description'
                 ),
                 dbc.Row([
                     dbc.Col(dbc.Label("Fecha"), md=4),
@@ -170,7 +144,6 @@ app.layout = dbc.Container([
         
     ),
     html.Div(
-    #dbc.Container(
         dbc.Container(
             dbc.Col(dcc.Graph(id="graph"), width=12),
             fluid=True
@@ -182,7 +155,7 @@ app.layout = dbc.Container([
     dbc.Card(
         dbc.CardBody(
             dbc.Row([
-                dbc.Col(md=9),
+                dbc.Col(md=6),
                 dbc.Col(
                     dbc.Button(
                         "Guardar",
@@ -190,34 +163,26 @@ app.layout = dbc.Container([
                         color="primary"),
                     md=3
                 ),
-                # dbc.Col(
-                #     dbc.Button(
-                #         "Show",
-                #         id='button_graph',
-                #         disabled = True,
-                #         color="primary"),
-                #     md=3                        
-                # ),
-                # dbc.Col(md=3)
+                dbc.Col([
+                    dbc.Button(
+                        "Ver datos",
+                        id='download',
+                        disabled = True,
+                        color="primary"),
+                    # Store inside the app joined dataframe
+                    dcc.Store(id='consolidate_data'),   
+                    # Download stored data
+                    dcc.Download(id="download-dataframe-csv")],
+                    md=3                        
+                ),
+                #dbc.Col(md=3)
             ])
                 
         )
             
-    )
+    ),
+
  ])
-
-# https://stackoverflow.com/questions/50213761/changing-visibility-of-a-dash-component-by-updating-other-component
-# @app.callback(
-#     Output(component_id='div_to_hide', component_property='style'),
-#     Output(component_id='div_to_show', component_property='style'),
-#     [Input(component_id='button_save', component_property='n_clicks')])
-
-# def show_hide_element(trigger):
-#     if trigger:
-#         return {'display': 'none'}, {'display': 'block'}
-#     else:
-#         return {'display': 'block'}, {'display': 'none'}
-
 
 
 
@@ -227,28 +192,25 @@ app.layout = dbc.Container([
     Input(component_id='drop_class', component_property='value')
     )
 def update_list(selec_class):
-    #book_class = classes_list[selec_class]
-    #sheet_estudiantes = 'Estudiantes'
-    #url_class = f"https://docs.google.com/spreadsheets/d/{book_class}/gviz/tq?tqx=out:csv&sheet={'Estudiantes'}"
-    #df_class= pd.read_csv(url_class)
     book_class_id = classes_list[selec_class]
     sheet_estudiantes = 'Estudiantes'
     book_class = gc.open_by_key(book_class_id)
     worksheet = book_class.worksheet('Estudiantes')
-    #url_class = f"https://docs.google.com/spreadsheets/d/{book_class}/gviz/tq?tqx=out:csv&sheet={'Estudiantes'}"
-    #df_class= pd.read_csv(url_class)
     df_class = pd.DataFrame(worksheet.get_all_records())  
+    df_class = df_class.sort_values(by=['nombre'])
     class_list = list(zip(df_class.codigo, df_class.nombre))
     class_dict = [{"label": k[1], "value": k[1]} for k in class_list]
     
     return class_dict
 
 @app.callback(
-    #Output("button_graph", "disabled"),
-    Output(component_id='check_students', component_property='value'),
-    Output(component_id='div_to_hide', component_property='style'),
-    Output(component_id='div_to_show', component_property='style'),
-    Output(component_id='graph', component_property='figure'),
+    Output("button_save", "disabled"),
+    Output("download", "disabled"),
+    Output('check_students', 'value'),
+    Output('div_to_hide', 'style'),
+    Output('div_to_show', 'style'),
+    Output('graph', 'figure'),
+    Output('consolidate_data', 'data'),
     [Input("button_save", "n_clicks")],    
     [Input('drop_class', 'value')],
     [Input('date-picker', 'date')],
@@ -263,11 +225,11 @@ def save_in_googlesheets(trigger,
     if trigger:
         if values == None:
             values = []
+        
+        # Open workbook of the class
         book_class_id = classes_list[selec_class]
         book_class = gc.open_by_key(book_class_id)
         worksheet = book_class.worksheet('Estudiantes')
-        #url_class = f"https://docs.google.com/spreadsheets/d/{book_class}/gviz/tq?tqx=out:csv&sheet={'Estudiantes'}"
-        #df_class= pd.read_csv(url_class)
         df_class = pd.DataFrame(worksheet.get_all_records())
         df = df_class[df_class['nombre'].isin(values)]
         df.insert(0, 'fecha', date_picked)
@@ -276,24 +238,65 @@ def save_in_googlesheets(trigger,
         df_values = df.values.tolist()
         # save in class id
         book_class = gc.open_by_key(book_class_id)
-        book_class.values_append('Faltas', {'valueInputOption': 'RAW'}, {'values': df_values})
+        book_class.values_append('Faltas', 
+                                 {'valueInputOption': 'RAW'}, 
+                                 {'values': df_values})
         # save in consolidate sheet
         book_consolidate = gc.open_by_key(book_consolidate_id)
-        book_consolidate.values_append('Consolidado', {'valueInputOption': 'RAW'}, {'values': df_values})
-        #return False, list()
-        # Create graph
+        book_consolidate.values_append('Consolidado', 
+                                       {'valueInputOption': 'RAW'}, 
+                                       {'values': df_values})
         
-        worksheet = book_consolidate.worksheet('Consolidado')
-        df_asistencia = pd.DataFrame(worksheet.get_all_records())
-        df_cum = df_asistencia.groupby("fecha").codigo.agg(conteo=('count'))
+        
+        df_joined = pd.concat([df_asistencia, df])
+        data_consolidated = df_joined.to_json(date_format='iso', orient='split')
+        
+        
+        df_cum = df_joined.groupby("fecha").codigo.agg(conteo=('count'))
         df_cum.reset_index(inplace=True)
         df_cum = df_cum.sort_values(by=['fecha'])
-       
-        fig = px.line(df_cum, x="fecha", y="conteo", title='Precio promedio de la habichuela en Medelín')
-
-        return list(), {'display': 'none'}, {'display': 'block'}, fig
+        
+        fig = px.line(df_cum, x="fecha", y="conteo", 
+                      title='Historico de Ausentismo')
+        fig.update_layout(title_x=0.5)
+        return True, False, list(), {'display': 'none'}, {'display': 'block'}, \
+            fig, data_consolidated
     else:
         return dash.no_update
+
+
+@app.callback(
+    Output("download-dataframe-csv", "data"),
+    Input("download", "n_clicks"),
+    Input('consolidate_data', 'data'),
+    prevent_initial_call=True,
+)
+def func(trigger, jsonified_data):
+    if trigger:
+        #df_asistencia = pd.read_json(jsonified_data, orient='split')
+        url_asistencia = f"https://docs.google.com/spreadsheets/d/{book_consolidate_id}/gviz/tq?tqx=out:csv"
+        df_asistencia= pd.read_csv(url_asistencia)
+        return dcc.send_data_frame(df_asistencia.to_csv, "asistencia.csv")
+    else:
+        return dash.no_update
+
+
+# app.clientside_callback(
+#     """
+#     function(trigger) {
+#         if trigger > 0:
+#             link = 'https://docs.google.com/spreadsheets/d/1w8h3dzb4dqSGDrj_QThuES7J_aqrAx_nwXgY7qUDSA0/edit?usp=sharing'
+#             window.open(link)
+#             return white_button_style
+#         else:
+#             return white_button_style
+#     }
+#     """,
+#     Output('download', 'style'),
+#     Input("download", "n_clicks"),
+#     prevent_initial_call=True,
+# )
+
 
 
 if __name__ == "__main__":
